4f82c66a293b0fa9d498851f4075fc24d600965a
Aaron Kovacs <aaronk4576@gmail.com>
2020-04-10 14:00:46 -0400

    Cache Graph Data

---
+++ source/models/graph_cache.py
@@ -0,0 +1,30 @@
+import base64
+import datetime
+import string
+import random
+import json
+import calendar
+
+from flask import current_app as app
+from flask import Flask, g, jsonify
+from flask_security import UserMixin, RoleMixin
+
+from sqlalchemy import desc
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship, backref, validates
+from sqlalchemy import Boolean, DateTime, Column, Integer, Float, String, ForeignKey, Text
+
+from passlib.hash import pbkdf2_sha256
+from itsdangerous import Serializer, JSONWebSignatureSerializer, BadSignature, BadData
+
+from ..database.base import Base
+from ..database.database import Session
+
+class GraphCache(Base):
+    __tablename__ = 'graph_cache'
+    id = Column(Integer(), primary_key=True)
+    country = Column(String(255))
+    json = Column(Text())
+    data_type = Column(String(255))
+    created = Column(DateTime(), default=datetime.datetime.utcnow)
+    last_updated = Column(DateTime(), default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
