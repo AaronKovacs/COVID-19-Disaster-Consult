14427e1d5af7f4eae2489fb6fc8ddab49ef5330b
armunday <63077174+armunday@users.noreply.github.com>
2020-04-18 16:16:21 -0400

    Update posts.py

--- source/views/posts.py
+++ source/views/posts.py
@@ -194,9 +194,11 @@ class CreatePost(Resource):
         post = session.query(Post).filter_by(id=postID).first()
         if post is None:
             post = Post()
-
+            
+        modifypost = content.replace("oembed url", "iframe width=\"560\" height=\"315\" src")
+		modifypost2 = modifypost.replace("oembed", "iframe")
         post.title = title
-        post.content = content
+        post.content = modifypost2
         post.public = public
 
         '''
